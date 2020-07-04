d2afbdaca0ba3559c875c99bef9e76579eacddbf
Jakub Olszewski <j.b.olszewski@gmail.com>
2020-07-04 10:23:50 +0200

    Teoria: Testy parametryzowane przez metodę

---
+++ src/test/java/pl/sdacademy/tarr2019java6/tdd/CalculatorParametizedJUnitParamsMethodTest.java
@@ -0,0 +1,46 @@
+package pl.sdacademy.tarr2019java6.tdd;
+
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author : Jakub Olszewski [http://github.com/jakub-olszewski]
+ * @project : tarr2019java4-tdd-project
+ * @since : 04.07.2020
+ **/
+@RunWith(JUnitParamsRunner.class)
+public class CalculatorParametizedJUnitParamsMethodTest {
+
+
+    private Object[] parametersToTestAdd(){
+        return new Object[]{
+                new Object[]{1,2,3},
+                new Object[]{2,3,5},
+                new Object[]{4,2,6}
+        };
+    }
+
+    @Test
+    @Parameters(method = "parametersToTestAdd")
+    public void testAdd(int numberA, int numberB, String expected){
+
+        // given
+        Calculator calc = new CalculatorImpl();
+
+        // when
+        calc.add(numberA,numberB);
+
+        // then
+        String message = String.format("Error %d + %d != %s because is equal %s",
+                numberA,numberB,calc.display(),expected);
+        assertEquals(message,expected,calc.display());
+    }
+
+    // TODO Zadanie: Zrobić test na odejmowanie stwórz metodę parametersToTestSub
+    // @Parameters(method = "parametersToTestSub")
+}
